70d5d80e8509d498a939f91c03353b4b4fbfc6fb
liorthr <liortahar116@gmail.com>
2024-05-20 20:47:47 +0300

    buil my own filter tag to translate any word using =hello_world=

--- mangalib/templatetags/custometags.py
+++ mangalib/templatetags/custometags.py
@@ -1,13 +1,17 @@
 from django import template
 from translate import Translator
-# from django.template.defaultfilters import stringfilter
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 tarnslator = Translator(to_lang='fr')
-# @register.filter
-# @stringfilter
-# def first_char(value):
-#     return value[0]
+##filter tag
+@register.filter
+@stringfilter
+def first_char(value):
+    return value[0]
+
+
+##filter tag made by me to translate any word 
 @register.simple_tag
 def hello_world(value):
     return tarnslator.translate(value)
