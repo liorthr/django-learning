166795d2068fb21717d6fa2cfea0a3cfd440f05b
liorthr <liortahar116@gmail.com>
2024-05-20 20:42:46 +0300

    balise and filter check$

--- mangalib/views.py
+++ mangalib/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import datetime
+
 def index(request):
     context = {
-        "message": 'Hello World ! ',
-        'message2': 'Juste un test'            
+        "message": 'Hello World ! ', 
+        'sentence': 'This is a test to fix my issue '       
                }
     template = loader.get_template('mangalib/index.html')
     return HttpResponse(template.render(context, request))
