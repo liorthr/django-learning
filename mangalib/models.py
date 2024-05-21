609ed3739b703335e1ec90410d37a60679891992
liorthr <liortahar116@gmail.com>
2024-05-21 20:46:22 +0300

    review of models

--- mangalib/models.py
+++ mangalib/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+"""
+    primary_key
+    unique
+    default
+    null
+    blank
+
+    ChargeField
+    IntegerField
+    DateField
+    EamilField
+    BooleanField
+"""
+class Author(models.Model):
+    authore_name = models.CharField(max_length = 64, unique = True)
+
+class Book(models.Model):
+    book_title = models.CharField(max_length = 32, unique = True)
+    book_quantity = models.IntegerField(default = 1)
+    author = models.ForeignKey(Author, on_delete = models.DO_NOTHING)
--- mangalib/views.py
+++ mangalib/views.py
@@ -5,9 +5,6 @@ from django.template import loader
 import datetime
 
 def index(request):
-    context = {
-        "message": 'Hello World ! ', 
-        'sentence': 'This is a test to fix my issue '       
-               }
+    context = {"message": 'Hello World ! '}
     template = loader.get_template('mangalib/index.html')
     return HttpResponse(template.render(context, request))
